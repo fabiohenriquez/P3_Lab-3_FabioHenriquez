09bf918149aef53bc2d523ad4082a4c3d846b70c
fabiohenriquez <fa.henriquez@hotmail.com>
2018-05-11 16:19:02 -0600

    Ejercicio 3

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,19 +1,28 @@
 #include<iostream>
 #include<string>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 using std::string;
 using std::cin;
 using std::cout;
-using std::endl
+using std::endl;
 
 ///METODOS
 int menu();
 void ejercicio1();
 void ejercicio2();
 void ejercicio3();
+void imprimirVector(int* arreglo,int size);
+void clearArray(int* arreglo);
+void ordenamiento(int* arreglo,int x ,int z, int y);
+void ordenamiento_final(int*arreglo, int x, int y);
+
+//using srand (time(NULL));
 
 
-int manin(){
+int main(){
 	char res='s';
 	do{
 	   switch(menu()){
@@ -65,8 +74,148 @@ void ejercicio1(){
 
 }
 		
+void ejercicio2(){
+	/*String polinomio;
+	cout<<"Ingrese el polinomio"<<endl;
+	cin>>polinomio;
 
-	
+	string delimitador_cuadrado = "X^2";
+	string delimitador_cuadrado_2 = "x^2";
+	string delimitador_1 = "X";
+	String delimitador_1_2 = "x";
+
+	string 
+*/
+
+
+
+
 
 }
+
+void ejercicio3(){
+
+	int size=0;
+        cout<<"Ingrese el tamaño del arreglo"<<endl;
+	cin>>size;
+
+	int vector[size];
+	int numero=0;
+	srand(time(NULL));
+
+	for(int i=0;i<size;i++){
+	
+		numero = rand()% 100 + 1;
+		vector[i] = numero;
+		//numero = numero + rand() % 100 +1;
+	
+	}
+	int c = sizeof(vector)/sizeof(vector[0]);	
+	imprimirVector(vector,c);
+
+	/*if(size == 0 || size ==1){
+          cout<<" "<<endl;
+	  imprimirVector(vector,size);
+	}*/
+
+	ordenamiento_final(vector,0, c-1);
+
+	imprimirVector(vector,c);
+
+
+
+}
+
+
+void imprimirVector(int*vector, int size){
+	for(int i= 0;i<size;i++){
+		cout<<vector[i]<<" ";
+
+	}
+	cout<<" "<<endl; 
+}
+
+void clearVector(int*Array){
+	delete[] Array;
+	Array=NULL;
+}
+
+void ordenamiento(int* arreglo, int x, int z, int y){
+	
+	int i;
+	int j;
+
+	int k;
+
+	int n1 = z-x+1;
+	int n2 = y-z;
+
+	int temporal [n1];
+	int temporal_2 [n2];
+
+	for(i=0;i<n1;i++)
+	   temporal[i] = arreglo[x+i];
+	
+
+	for(j=0;j<n2;j++)
+	   temporal_2[j] = arreglo[z+1+j]; 
+	
+	
+	i =0;
+	j=0;
+	k=x;
+
+	while(i<n1&& j<n2){
+
+		if(temporal[i]<= temporal_2[j]){
+			arreglo[k] = temporal[i];
+			i++;
+		}else{
+
+		 arreglo[k] = temporal_2[j];
+		 j++;
+
+		}
+		k++;
+
+	}
+
+	while(i<n1){
+		arreglo[k] = temporal[i];
+		i++;
+		k++;
+	}
+
+	while(j<n2){
+	 arreglo[k] = temporal_2[j];
+	 j++;
+	 k++;
+
+	}
+
+
+	}
+
+
+         
+
+
+void ordenamiento_final(int*arreglo, int l, int r){
+	if(l<r){
+	int m = l +(r-l)/2;
+
+	ordenamiento_final(arreglo,l,m);
+
+	ordenamiento_final(arreglo,m+1,r);
+
+	ordenamiento(arreglo,l,m,r);
+
+
+
+
+	}
+
+}
+
+
 
